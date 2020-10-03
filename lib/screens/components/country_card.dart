1d19e754d7f0324534cdc866265ebc2447e833ce
mamun <mh72156@gmail.com>
2020-10-03 15:42:57 +0600

    wow

--- lib/screens/components/country_card.dart
+++ lib/screens/components/country_card.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_clock/size_config.dart';
 import 'package:flutter_clock/size_config_two.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
